12e1d882e689ac60af176c229302d3811349ce47
Devvart Poddar <poddardevvart@gmail.com>
2020-11-19 20:10:10 +0530

    Initialisation
    Inititialising package

---
+++ R/significance_tests.R
@@ -0,0 +1,158 @@
+#' test_significance
+#'
+#' Internal function to test significance of results
+#'
+#' @param .data Summarised dataframe
+#' @param compare Group::response combination to be used for comparison
+#' 
+#' @return A dataframe of summarised results for numeric variables
+#' 
+#' #' @importFrom magrittr "%>%"
+test_significance <- function(.data,
+                              compare = compare) {
+
+  # Function to test significance of a given mean against comparable or default values. Uses welch t test
+  # TODO: Change default compare row by type; if numeric use the overall row, and if categorical,
+  # use the row that meets the response
+
+  # CHECK if data is 0 rows, return data
+  if (nrow(.data) == 0) return(.data)
+
+  # convert compare to lowercase, and trim ws
+  compare <- trimws(compare) %>%
+    tolower()
+
+  # if compare is overall, pre-pend with "overall::"
+  if (compare == "overall") {
+    compare <- .data %>%
+      dplyr::filter(group == "Overall") %>%
+      dplyr::pull(response) %>%
+      unique() %>%
+      trimws() %>%
+      tolower() %>%
+      paste0("overall::", .)
+  }
+
+  # compare <- str_split(compare, "::") %>%
+  #   unlist()
+
+  # comparison row
+  compare_row <- .data %>%
+    dplyr::mutate(dplyr::across(dplyr::all_of(c("group", "response")), ~tolower(trimws(.)))) %>%
+    dplyr::mutate(tmp = glue::glue("{group}::{response}")) %>%
+    dplyr::filter(tmp %in% compare) %>%
+    dplyr::select(tmp_response = response,
+           compareN = N, compareV = value, compareVSE = value_se)
+
+  # if compare row is null, push out a warning and use overall instead
+  if (nrow(compare_row) == 0) {
+    rlang::warn(glue::glue("The selected comparision does not exist in the data, defaulting to overall"))
+
+    return(test_significance(.data, "overall"))
+  }
+
+  # If only one row is selected, it should be matched to all. If not then match by response
+  if (nrow(compare_row) == 1) {
+    # If only one single row is slected to be compared againgst
+    significance_data <- .data %>%
+      dplyr::mutate(compareN = compare_row$compareN,
+             compareV = compare_row$compareV,
+             compareVSE = compare_row$compareVSE) %>%
+      dplyr::mutate(tmp_group = tolower(group),
+             tmp_response = tolower(response),
+             tmp_match = tolower(glue::glue("{tmp_group}::{tmp_response}")))
+  } else {
+    significance_data <- .data %>%
+      dplyr::mutate(tmp_group = tolower(group),
+             tmp_response = tolower(response),
+             tmp_match = tolower(glue::glue("{tmp_group}::{tmp_response}"))) %>%
+      dplyr::left_join(compare_row, by = c("tmp_response"))
+  }
+
+  significance_data <- significance_data %>%
+    dplyr::mutate(pvalue = purrr::pmap_dbl(list(N,
+                                  value,
+                                  value_se,
+                                  compareN,
+                                  compareV,
+                                  compareVSE), function(n, m, se, n0, m0, se0) {
+      tmp <- welch_ttest(
+        m1 = m,
+        m2 = m0,
+        s1 = se,
+        s2 = se0,
+        n1 = n,
+        n2 = n0
+      )
+
+      tmp$p_value
+      }),
+      sgnf = dplyr::case_when(
+        # Highlight row being compared against
+        tmp_match %in% compare ~ "<<--|",
+
+        # Significance stars
+        pvalue < 0.001 ~ "[***]",
+        pvalue < 0.05  ~ "[ **]",
+        pvalue < 0.1   ~ "[  *]",
+        TRUE           ~ "-----"
+      )
+    ) %>%
+    dplyr::select(-tmp_group,
+                -tmp_response,
+                -tmp_match,
+                -compareN,
+                -compareV,
+                -compareVSE)
+
+  return(significance_data)
+}
+
+#' Welch T test for unequal variances
+#'
+#' Internal function to summarise numeric variables
+#'
+#' @param m1 Mean of group 1
+#' @param m2 Mean of group 2
+#' @param s1 Standard deviation of group 1
+#' @param s2 Standard deviation of group 2
+#' @param n1 N for group 1
+#' @param n2 N for group 2
+#' @param m0 Diff of means to be compared against. Defaults to 0
+#' @param equal.variance Are variances of the two groups to be taken as the same
+#' 
+#' @return A dataframe of pvalues and means
+welch_ttest <- function(m1,
+                         m2,
+                         s1,
+                         s2,
+                         n1,
+                         n2,
+                         m0 = 0,
+                         equal.variance = FALSE) {
+
+  # correct for providing SE instead of SD
+  s1 <- s1 * sqrt(n1)
+  s2 <- s2 * sqrt(n2)
+
+  if (equal.variance == FALSE) {
+    se <- sqrt( (s1^2/n1) + (s2^2/n2) )
+    # welch-satterthwaite df
+    df <- ((s1^2/n1 + s2^2/n2)^2)/((s1^2/n1)^2/(n1 - 1) + (s2^2/n2)^2/(n2 - 1))
+  } else {
+    # pooled standard deviation, scaled by the sample sizes
+    se <- sqrt((1/n1 + 1/n2) * ((n1 - 1) * s1^2 + (n2 - 1) * s2^2)/(n1 + n2 - 2) )
+    df <- n1 + n2 - 2
+  }
+
+  t <- (m1 - m2 - m0)/se
+
+  dat = tibble::tibble(
+    difference_of_means = (m1 - m2),
+    std_error = se,
+    t_value = t,
+    p_value = 2 * pt(-abs(t), df)
+  )
+
+  return(dat)
+}
---
+++ R/summariser.R
@@ -0,0 +1,327 @@
+#' Summarise survey data
+#'
+#' This function provides a summary of survey data based on the design provided,
+#' as well as checks for significance of results
+#'
+#' @param .data Dataframe or survey data to be summarised
+#' @param var Variable within dataframe to be summarised
+#' @param ... Variables to group by
+#' @param stat One of "mean", "median" or "total". Defaults to mean if not correctly provided 
+#' @param compare Stat to use for significance tests of survey results. Must be provided in "group::response" format. Defaults to the overall value
+#' @param survey.design List of survey design inputs to be applied to dataframe, if not survey data of class 'tbl_svy'. Please see '?survey::svydesign' to identify required inputs
+#' @param ci_level confidence level to be summarised for
+#' @param simplify Should the survey results be simplified from decimals to numbers?
+#' 
+#' @return A dataframe of summarised results
+#' 
+#' @importFrom magrittr "%>%"
+#' 
+#' @export
+summariser <- function(.data,
+                       var,
+                       ...,
+                       # allowed stats to ask for
+                       stat = c("mean", "median", "total"),
+                       # metric to compare significance against
+                       compare = "overall",
+                       # Optional list of survey design inputs
+                       survey.design = NULL,
+                       # CI level to be used
+                       ci_level = 0.95,
+                       # Simplify results by converting to percentage
+                       simplify = TRUE) {
+
+  # List of checks for inputs ----
+  # 1 | Data or the variable are not missing
+  if (missing(.data) | missing(var))
+    rlang::abort("Please provide a valid data or variable input to summarise")
+
+  # Enquoting selected variable
+  enquo_var <- rlang::enquo(var)
+
+  # 2 | Check if variable is present in data
+  tmp <- try(tidyselect::eval_select(enquo_var,
+            data = tibble::as_tibble(.data)),
+            silent = TRUE)
+  if (class(tmp) == "try-error")
+    rlang::abort(glue::glue("Column {rlang::quo_text(enquo_var)} does not exist in the dataframe"))
+
+  enquo_var <- names(tmp)
+  rm(tmp)
+
+  # Quoting grouping variables
+  grouped_vars <- rlang::expr(c(...))
+
+  # 3 | Check if group variables are present in data
+  tmp <- try(tidyselect::eval_select(grouped_vars,
+            data = tibble::as_tibble(.data)),
+            silent = TRUE)
+  if (class(tmp) == "try-error")
+    rlang::abort(glue::glue("Grouping vars provided do not exist in the dataframe. Please check again"))
+
+  grouped_vars <- names(tmp)
+  rm(tmp)
+
+  # 4 | Check if argument for stat matches default values
+  stat <- match.arg(stat)
+
+  # 5 | Check if compare is a string
+  if (!purrr::is_character(compare))
+    rlang::abort("`compare` can only take a string of the form {group}::{response} in order to estimate significance")
+
+  # 6 | Set up survey if design is not null
+  if (!is.null(survey.design)) {
+
+    # 6a | Check if variables provided in survey.design match required inputs
+    argsList <- c("ids", "probs", "strata", "fpc", 
+                "weights", "nest", "check.strata")
+    if (all(names(survey.design) %in% argsList))
+      rlang::abort("List of inputs provided in survey design do not match inputs. Please see '?survey::svydesign' to identify required inputs")
+
+    rm(argsList)
+
+    # 6b | Check if variables provided in survey.design are present in the dataframe
+    # TODO
+
+    # Set up .data based on survey design
+    .data <- do.call(srvyr::as_survey,
+                    c(list(.data),
+                      survey.design))
+
+  }
+
+  # 7 | Check if data inherits class accepted by srvyr data
+  if (!inherits(.data, "tbl_svy"))
+    rlang::abort("Data is not defined as a survey data of class 'tbl_svy'. Please provide a survey.design, or data set up as a survey")
+
+  # 8 | Checks for significance numbers and values
+  # 8a | Is the CI level numeric
+  if (!is.numeric(ci_level)) {
+    rlang::warn("Please provide a numeric value for ci_level. Defaulting to 0.95")
+
+    ci_level <- 0.95
+  }
+
+  # 8b | Is the CI level less than or equal to one
+  if (any(ci_level > 1)) {
+    rlang::warn("CI levels is greater than 1. Defaulting to 0.95")
+
+    ci_level <- 0.95
+  }
+
+  # 8c | Has only one CI level been provided
+  if (length(ci_level) > 1) {
+    rlang::warn("More than one CI levels are provided. Defaulting to first value")
+
+    ci_level <- ci_level[1]
+  }
+
+  # Define local variables ----
+  # 1 | Is the variable numeric or logical
+  IS_NUMERIC <- .data %>%
+    dplyr::pull(enquo_var) %>%
+    inherits(c("integer", "numeric", "logical"))
+
+  # Summarising variables ----
+  if (IS_NUMERIC) {
+    .data <- summariser_numeric(.data,
+                                 enquo_var,
+                                 grouped_vars,
+                                 stat,
+                                 ci_level)
+  } else {
+    .data <- summariser_character(.data,
+                                   enquo_var,
+                                   grouped_vars,
+                                   stat,
+                                   ci_level)
+  }
+
+  # Test significance ----
+  .data <- test_significance(.data, compare)
+
+  # Simplify ----
+  if (simplify){
+    # 1 | if is character, multiply by 100
+    if (!IS_NUMERIC) {
+      .data <- .data %>%
+        dplyr::mutate(dplyr::across(starts_with("value"), ~ .x*100))
+    }
+
+    .data <- .data %>%
+      dplyr::mutate(dplyr::across(where(is.numeric), ~round(.x, 2)))
+  }
+
+  # Return values ----
+  return(.data)
+}
+
+#' summariser_numeric
+#'
+#' Internal function to summarise numeric variables
+#'
+#' @param .data Dataframe or survey data to be summarised
+#' @param var Variable within dataframe to be summarised
+#' @param group Variables to group by
+#' @param stat One of "mean", "median" or "total". Defaults to mean if not correctly provided 
+#' @param level confidence level to be summarised for
+#' 
+#' @return A dataframe of summarised results for numeric variables
+#' 
+#' #' @importFrom magrittr "%>%"
+summariser_numeric <- function(.data, var, group, stat, level) {
+  # Summarises variables if they are numeric or logical in nature
+
+  # Check if the variable is logical
+  lgl <- .data %>%
+    dplyr::pull(var) %>%
+    inherits(c("logical"))
+
+  if (lgl) {
+    # If logical, convert it to numeric and print out a message
+    .data <- .data %>%
+      dplyr::mutate(!!rlang::sym(var) := !!rlang::sym(var) * 1)
+
+    rlang::inform(glue::glue("Variable {var} has been converted from logical to numeric. Please force as
+                       character if you wish to keep both categories"))
+  }
+
+  # creating summary function
+  sum_fnc <- switch (stat,
+    "mean"   = srvyr::survey_mean,
+    "median" = srvyr::survey_median,
+    "total"  = srvyr::survey_total,
+    srvyr::survey_mean
+  )
+
+  # Ungrouped summary
+  summary_data <- .data %>%
+    srvyr::summarise(value = sum_fnc(!!rlang::sym(var),
+                              vartype = c("se", "ci"),
+                              na.rm = TRUE,
+                              level = level,
+                              # If the variable is converted from logical, yes else no
+                              proportion = lgl),
+              N = srvyr::unweighted(dplyr::n())) %>%
+    srvyr::mutate(group = "Overall",
+           response = "Overall") %>%
+    srvyr::select(group, response, N, value, everything())
+
+  # Grouped summary
+  if (length(group) > 0) {
+    # If a group is present, do a grouped summary
+    grouped_data <- purrr::map_dfr(group, function(group_var) {
+      .data %>%
+        srvyr::group_by(!!rlang::sym(group_var), .add = FALSE) %>%
+        srvyr::summarise(value = sum_fnc(!!rlang::sym(var),
+                                  vartype = c("se", "ci"),
+                                  na.rm = TRUE,
+                                  level = level,
+                                  # If the variable is converted from logical, yes else no
+                                  proportion = lgl),
+                  N = srvyr::unweighted(dplyr::n())) %>%
+        srvyr::mutate(group = group_var,
+               response = !!rlang::sym(group_var)) %>%
+        srvyr::select(group, response, N, value, everything(), -!!sym(group_var))
+      }
+    )
+
+    summary_data <- dplyr::bind_rows(summary_data,
+                              grouped_data)
+  }
+
+  return(summary_data)
+}
+
+#' summariser_character
+#'
+#' Internal function to summarise character variables
+#'
+#' @param .data Dataframe or survey data to be summarised
+#' @param var Variable within dataframe to be summarised
+#' @param group Variables to group by
+#' @param stat One of "mean", "median" or "total". Defaults to mean if not correctly provided 
+#' @param level confidence level to be summarised for
+#' 
+#' @return A dataframe of summarised results for numeric variables
+#' 
+#' #' @importFrom magrittr "%>%"
+summariser_character <- function(.data, var, group, stat, level) {
+  # Summarises variable if they are character or factor in nature
+
+  # creating summary function
+  sum_fnc <- switch (stat,
+                     "mean"  = srvyr::survey_mean,
+                     # There is no median for characters or factors. Defaulting to mean
+                     "total" = srvyr::survey_total,
+                     srvyr::survey_mean
+  )
+
+  # If stat is median, inform saying that it is not gonna happen
+  if (stat == "median")
+    rlang::warn("`stat` median is not allowed with character vectors, providing survey mean instead")
+
+  # Sanity checks
+  # 1. Converting NAs in var and group var to character
+  .data <- .data %>%
+    srvyr::mutate(srvyr::across(srvyr::any_of(c(var, group)), stringr::str_replace_na))
+
+  # Ungrouped summary
+  # Creating list of responses
+  list_of_responses <- .data %>%
+    srvyr::pull(var) %>%
+    unique()
+
+  # Iterating over list of responses, with 1 / 0 variables
+  summary_data <- purrr::map_dfr(list_of_responses, function(x) {
+    .data %>%
+      srvyr::mutate(`____TEMPVAR____` = (!!rlang::sym(var) == x) * 1) %>%
+      srvyr::summarise(value = sum_fnc(`____TEMPVAR____`,
+                                vartype = c("se", "ci"),
+                                na.rm = TRUE,
+                                level = level,
+                                proportion = TRUE),
+                N = srvyr::unweighted(sum(`____TEMPVAR____`))) %>%
+      srvyr::mutate(group = "Overall",
+             response = x) %>%
+      srvyr::select(group, response, N, value, everything())
+  })
+
+  # Grouped summary
+  if (length(group) > 0) {
+    grouped_data <- purrr::map_dfr(group, function(group_var) {
+
+      list_of_responses <- .data %>%
+        dplyr::pull(var) %>%
+        unique()
+
+      # Summarising
+      purrr::map_dfr(list_of_responses, function(x) {
+        .data %>%
+          srvyr::group_by(!!rlang::sym(group_var)) %>%
+          srvyr::mutate(`____TEMPVAR____` = (!!rlang::sym(var) == x) * 1) %>%
+          srvyr::summarise(value = sum_fnc(`____TEMPVAR____`,
+                                    vartype = c("se", "ci"),
+                                    na.rm = TRUE,
+                                    level = level,
+                                    proportion = TRUE),
+                    N = srvyr::unweighted(sum(`____TEMPVAR____`))) %>%
+          srvyr::mutate(group = case_when(
+            # If more than one groups are provided, attach a group name finder, else just the normal works
+            length(group) > 1 ~ paste0(group_var, " | ", !!rlang::sym(group_var)),
+            TRUE ~ as.character(!!sym(group_var))
+            ),
+            response = x) %>%
+          srvyr::select(group, response, N, value, everything(), - !!rlang::sym(group_var))
+        }
+        ) %>%
+        dplyr::arrange(group)
+      }
+    )
+
+    summary_data <- dplyr::bind_rows(summary_data,
+                              grouped_data)
+  }
+
+  return(summary_data)
+}
